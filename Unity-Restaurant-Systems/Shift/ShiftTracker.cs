dc9ee1b2f86e4b9387d9a140b30a81635ab02808
Zeki Can Biçim <148773813+TitaNs17@users.noreply.github.com>
2026-09-11 15:50:10 +0300

    Promote shift tracker out of V2

---
+++ Unity-Restaurant-Systems/Shift/ShiftTracker.cs
@@ -0,0 +1,32 @@
+namespace UnityGameSystems.Restaurant.Shift
+{
+    public sealed class ShiftTracker
+    {
+        private readonly ShiftSummary summary = new();
+
+        public ShiftSummary Summary => summary;
+
+        public void RecordOrderReceived() => summary.OrdersReceived++;
+
+        public void RecordOrderCompleted(decimal revenue, int customers = 1)
+        {
+            summary.OrdersCompleted++;
+            summary.CustomersServed += customers > 0 ? customers : 0;
+            summary.Revenue += revenue > 0m ? revenue : 0m;
+        }
+
+        public void RecordOrderCancelled() => summary.OrdersCancelled++;
+
+        public void RecordExpense(decimal amount)
+        {
+            if (amount > 0m)
+                summary.Expenses += amount;
+        }
+
+        public void RecordWaste(decimal amount)
+        {
+            if (amount > 0m)
+                summary.WasteCost += amount;
+        }
+    }
+}
