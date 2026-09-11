80586b1e05b06b3f8ae01da7ee7392993293b70d
Zeki Can Biçim <148773813+TitaNs17@users.noreply.github.com>
2026-09-11 09:05:29 +0300

    Add quest data definitions

---
+++ Unity-Gameplay-Systems/Quests/QuestDefinition.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+namespace UnityGameSystems.Quests
+{
+    [Serializable]
+    public struct QuestObjective
+    {
+        public string id;
+        public string description;
+        [Min(1)] public int requiredAmount;
+    }
+
+    [CreateAssetMenu(menuName = "Game/Quest", fileName = "Quest")]
+    public sealed class QuestDefinition : ScriptableObject
+    {
+        [SerializeField] private string questId;
+        [SerializeField] private string title;
+        [SerializeField] private QuestObjective[] objectives;
+
+        public string Id => questId;
+        public string Title => title;
+        public QuestObjective[] Objectives => objectives;
+    }
+}
